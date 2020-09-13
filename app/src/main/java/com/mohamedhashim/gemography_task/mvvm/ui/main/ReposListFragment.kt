56b5e55c2e7b370e99d0f903ab79048503b7306a
MohamedHashim <sawmido2000@gmail.com>
2020-09-13 20:28:42 +0200

    Integrate the Navigation component and launch the reposListFragment

--- app/src/main/java/com/mohamedhashim/gemography_task/mvvm/ui/main/ReposListFragment.kt
+++ app/src/main/java/com/mohamedhashim/gemography_task/mvvm/ui/main/ReposListFragment.kt
@@ -1,7 +1,22 @@
 package com.mohamedhashim.gemography_task.mvvm.ui.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mohamedhashim.gemography_task.R
+
 /**
  * Created by Mohamed Hashim on 9/13/2020.
  */
-class ReposListFragment {
+class ReposListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       return inflater.inflate(R.layout.fragment_repos_list, container,false)
+    }
 }
